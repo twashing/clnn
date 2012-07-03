836b3dc2cc9bf1b4b5b739c50923532b90ca3f0f
Timothy Washington <twashing@gmail.com>
2012-07-02 17:55:51 -0700

    -- adding swank -- can pull in config and test data file and

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject nn "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [swank-clojure "1.4.2"]
+                 [clojure-csv/clojure-csv "2.0.0-alpha1"]]
+  :plugins [[lein-swank "1.4.4"]]
   :dev-dependencies [[lein-midje "1.0.10"]
-                     [com.stuartsierra/lazytest "1.2.3"]]
+                     [com.stuartsierra/lazytest "1.2.3"]
+                     ]
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
 )
--- src/nn/core.clj
+++ src/nn/core.clj
@@ -1,7 +1,20 @@
-(ns nn.core)
+(ns nn.core
+  
+  (:require [clojure.pprint :as pprint]
+            [clojure-csv.core :as csv]
+            [clojure.java.io :as io]
+  )
+  
+)
 
 
 ;; consume test data (from config)
+(let [config (load-file "etc/config.clj")
+      dname (-> config :data :test)
+     ]
+
+  (pprint/pprint (csv/parse-csv (io/reader dname)))
+)
 
 
 ;; create neuron protocol (using jodatime)
