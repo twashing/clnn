dd80909f4b039517508d74b8fb6d6ef46f4446c4
Timothy Washington <twashing@gmail.com>
2012-08-10 21:38:17 -0700

    -- calculating input and hidden layer values for first neuron

--- src/nn/neuralnet.clj
+++ src/nn/neuralnet.clj
@@ -6,20 +6,23 @@
 )
 
 
+;; NORMALIZE INPUT DATA
+(defn normalize-data [multiplier data-double]
+  (/ data-double multiplier)
+)
+
+
+
 ;; INPUT LAYER
 (defn get-time-format []
   (cformat/formatter "dd.MM.yyy HH:mm:ss.SSS")
 )
 (defn create-input-neuron [key value]
-
   {:key key
    :value value
-
   }
 )
-(defn normalize-data [data-double multiplier]
-  (/ data-double multiplier)
-)
+
 (defn create-input-layer
   "Creating a neuron for each input value"
   [inputs]
@@ -28,11 +31,11 @@
         tformat (get-time-format)
        ]
     (-> input-layer
-        (conj (create-input-neuron :time (cformat/parse tformat (first inputs))))
-        (conj (create-input-neuron :bid (Double/parseDouble (second inputs))))
-        (conj (create-input-neuron :ask (Double/parseDouble (nth inputs 2))))
-        (conj (create-input-neuron :bvolume (Double/parseDouble (nth inputs 3))))
-        (conj (create-input-neuron :avolume (Double/parseDouble (nth inputs 4))))
+        (conj (create-input-neuron :time (->> (first inputs) (cformat/parse tformat) ccoerce/to-long double (normalize-data 1000000000000) )))
+        (conj (create-input-neuron :bid (-> (second inputs) Double/parseDouble )))
+        (conj (create-input-neuron :ask (-> (nth inputs 2) Double/parseDouble )))
+        (conj (create-input-neuron :bvolume (->> (nth inputs 3) Double/parseDouble (normalize-data 1000000) )))
+        (conj (create-input-neuron :avolume (->> (nth inputs 4) Double/parseDouble (normalize-data 1000000) )))
     )
   )
 )
@@ -61,15 +64,11 @@
 (defn linear-combiner
   [neuron]
   
-  (println (str "linear-combiner function CALLED > " (-> neuron :inputs first :value ccoerce/to-long) ))
+  (println (str "linear-combiner function CALLED > " (-> neuron :inputs)))
 
   ;; if a number, just multiply and add to the result; otherwise, convert data to long
-  (reduce #(+ %1 (* (if(number? (:value %2))
-                       (:value %2)
-                       (ccoerce/to-long (:value %2))
-                    )
-                    (:weight %2))
-          )
+  (reduce #(+ %1 (* (:value %2)
+                    (:weight %2)))
           0
           (:inputs neuron)
   )
@@ -83,4 +82,24 @@
   ;;(println "activation function CALLED")
   (/ 1 (+ 1 (incanter/exp (* -1 value))))
   
+)
+
+
+(require '[nn.config.config :as config])
+(defn run-test[]
+
+  (let [train-data (config/load-train-data)
+        input-layer (create-input-layer (second train-data))
+        hidden-layer (create-hidden-layer input-layer)]
+    
+    (println (str "Input Layer: " input-layer))
+    ;(map #(neuralnet/linear-combiner %1) hidden-layer)
+
+    ;; apply linear combiner and add the bias to get a value
+    (let [value (+ (linear-combiner (first hidden-layer))
+                   (:bias (first hidden-layer)))]
+      (println (str "Hidden Neuron value: " value))
+      (println (str "Hidden Neuron activation-value: " (activation value)))
+    )
+  )
 )
