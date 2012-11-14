c0d40153e530279eb58124202aa1e2625317ba59
Timothy Washington <twashing@gmail.com>
2012-11-13 21:33:14 -0800

    -- modifying feed-forward calculations of input layer

--- src/nn/layers/input.clj
+++ src/nn/layers/input.clj
@@ -22,16 +22,17 @@
 
 (defn create-input-neuron [input-map]
   
-  (merge input-map { :id (util/generate-id) })
+  (merge input-map { :weight (rand)
+                     :id (util/generate-id) })
 )
 (defn create-input-layer
   "Creating a neuron for each input value"
   [inputs]
-
+  
   (let [input-layer '()
         tformat (get-time-format)
        ]
-    [ (create-input-neuron { :key :time :value (->> (first inputs) (cformat/parse tformat) ccoerce/to-long double (normalize-data 1000000000000) ) } )
+    [(create-input-neuron { :key :time :value (->> (first inputs) (cformat/parse tformat) ccoerce/to-long double (normalize-data 1000000000000) ) } )
       (create-input-neuron { :key :bid :value (-> (second inputs) Double/parseDouble ) } )
       (create-input-neuron { :key :ask :value (-> (nth inputs 2) Double/parseDouble ) } )
       (create-input-neuron { :key :bvolume :value (->> (nth inputs 3) Double/parseDouble (normalize-data 1000000) ) } )
@@ -40,70 +41,18 @@
   )
 )
 
-#_(defn create-input-neuron [inputs]
-  
-  {:inputs (reduce #(conj %1 { :key (:key %2) :value (:value %2) :weight (rand) :bias 0 }) '() inputs)
-   :id (util/generate-id)
-  }
-)
-#_(defn create-input-layer
-  "Creating a neuron for each input value"
-  [inputs]
-
-  (let [input-layer '()
-        tformat (get-time-format)
-        input-list [
-          { :key :time :value (->> (first inputs) (cformat/parse tformat) ccoerce/to-long double (normalize-data 1000000000000) ) }
-          { :key :bid :value (-> (second inputs) Double/parseDouble ) }
-          { :key :ask :value (-> (nth inputs 2) Double/parseDouble ) }
-          { :key :bvolume :value (->> (nth inputs 3) Double/parseDouble (normalize-data 1000000) ) }
-          { :key :avolume :value (->> (nth inputs 4) Double/parseDouble (normalize-data 1000000) ) }
-        ]
-       ]
-    (-> input-layer
-        (conj (create-input-neuron input-list ))
-        (conj (create-input-neuron input-list ))
-        (conj (create-input-neuron input-list ))
-        (conj (create-input-neuron input-list ))
-        (conj (create-input-neuron input-list ))
-    )
-  )
-)
-
 
 ;; CALCULATE VALUES
-(defn calculate-leaf-value [neural-layer]
-  
-  (loop [loc (layers/create-zipper neural-layer)]
-    
-    (if (zip/end? loc)
-      (zip/root loc)
-      (if (and  (-> loc zip/node map?) 
-                (-> loc zip/node (contains?   :key)))
-        (recur  (zip/next
-                  (zip/edit loc merge
-                    
-                    (let  [ val (:value (zip/node loc))
-                            wei (:weight (zip/node loc))
-                            calculated (* val wei) ]
-                          { :calculated calculated })
-                    
-                  ))) 
-        (recur (zip/next loc))
-      )
-    ) 
-  )
-)
-
-(defn calculate-final-value [ech-map]
-  (merge ech-map  { :calculated-value (-> ech-map layers/calculate-linear-combiner layers/calculate-activation)
-                  }
-  )
-)
 (defn calculate-value [neural-layer]
   
-  ;; first calculate leaf values, then map calculated-values over the result list
-  (map calculate-final-value (calculate-leaf-value neural-layer))
+  (map (fn [ech]
+         (let  [val (:value ech)
+                wei (:weight ech)
+                calculated (* val wei) ]
+           (println ech)
+           (merge ech { :calculated-value calculated })))
+       neural-layer
+  )
 )
 
 
